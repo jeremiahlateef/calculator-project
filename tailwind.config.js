e036bcbbd595a5848b8c58575df42aeee103b9ea
unknown <lateefjeremiah@gmail.com>
2022-11-07 22:28:38 +0100

    create tailiwinf app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "9769x",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
